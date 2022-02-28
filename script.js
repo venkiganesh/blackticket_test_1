1e4f39b5ab950b0ead7632a2db34060544e87af8
Venkatesh <76658782+venkiganesh@users.noreply.github.com>
2022-03-01 00:32:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openmenu() {
+  let x = document.getElementById('navbartoggle');
+  x.style.animation="navopen 0.5s  forwards linear";
+
+}
+function closemenu() {
+  let x = document.getElementById('navbartoggle');
+  x.style.animation="navclose 0.5s  forwards linear";
+}
